a23f63268210e889f9242625f1d8dbb1d01ede86
codeforges <ruben@codeforges.com>
2019-10-26 19:03:05 +0200

    Inventory

---
+++ Assets/Scripts/Entities/RocketPartController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Entities
+{
+    public class RocketPartController: MonoBehaviour
+    {
+        public RocketPart part;
+
+        private void Start()
+        {
+            print(part.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/RocketPartsInventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Containers.RocketParts;
+using Entities;
+using UnityEngine;
+
+public class RocketPartsInventory : MonoBehaviour
+{
+    private List<RocketPart> availableParts = new List<RocketPart>();
+    
+    void Start()
+    {
+        spawnAllParts();
+    }
+
+    public List<RocketPart> getAvailableObject()
+    {
+        return availableParts;
+    }
+    
+    private void spawnAllParts()
+    {
+        var items = RocketPartsDatabase.Instance.rocketParts;
+        var lastPrefabSize = Vector3.zero;
+        availableParts = items;
+        
+        for (int i = 0; i < items.Count; i++)
+        {
+            var partPrefab = items[i].model;
+            var instance = Instantiate(partPrefab, transform);
+            var size = instance.GetComponent<Collider>().bounds.size;
+
+            if (i > 0)
+            {
+                instance.transform.position += new Vector3(size.x + lastPrefabSize.x, 0, 0);
+            }
+
+            instance.AddComponent<RocketPartController>();
+            instance.GetComponent<RocketPartController>().part = items[i];
+            
+            lastPrefabSize = size;
+        }
+    }
+}
