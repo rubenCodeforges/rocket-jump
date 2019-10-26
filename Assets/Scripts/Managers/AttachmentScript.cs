8a2b6085d28372f4426bed423b501c4f77e3b142
codeforges <ruben@codeforges.com>
2019-10-26 20:03:36 +0200

    Attachments system

--- Assets/Scripts/Managers/AttachmentScript.cs
+++ Assets/Scripts/Managers/AttachmentScript.cs
@@ -1,28 +1,49 @@
-﻿using Enums;
+﻿using Containers;
+using Entities;
+using Enums;
 using UnityEngine;
 
 public class AttachmentScript : MonoBehaviour
 {
+    public RocketController rocketController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){ 
+        if (Input.GetMouseButtonDown(0))
+        {
             Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)){
-               print(hit.transform.name);
+            if (Physics.Raycast(ray, out hit))
+            {
+                var rocketPart = hit.transform.gameObject.GetComponent<RocketPartController>();
+                if (rocketPart != null)
+                {
+                    ApplyAttachment(rocketPart);
+                }
             }
         }
     }
 
-    public void OnUserInput(InputDirection direction)
+    private void ApplyAttachment(RocketPartController rocketPart)
     {
-        print(direction);
+        var thrust = rocketPart.part.thrust;
+        var type = rocketPart.part.type;
+
+        if (type == PartType.THRUSTER)
+        {
+            rocketController.thrust = thrust;
+        }
+        else if (type == PartType.RCS)
+        {
+            rocketController.rcsThrust = thrust;
+        }
+
+        print(rocketPart.part.thrust);
     }
 }
