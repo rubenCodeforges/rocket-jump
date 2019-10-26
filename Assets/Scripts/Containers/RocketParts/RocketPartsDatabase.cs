9ea2ae37c97876dd1042f433f52aba28680fa5e4
codeforges <ruben@codeforges.com>
2019-10-26 13:39:54 +0200

    Object spawner and Database

--- Assets/Scripts/Containers/RocketParts/RocketPartsDatabase.cs
+++ Assets/Scripts/Containers/RocketParts/RocketPartsDatabase.cs
@@ -7,11 +7,21 @@ namespace Containers.RocketParts
     public class RocketPartsDatabase : MonoBehaviour
     {
         public List<RocketPart> rocketParts = new List<RocketPart>();
+        public static RocketPartsDatabase Instance { get; private set; }
         private const string _xmlPath = "configs/items";
 
         void Awake()
         {
-            rocketParts = RocketPartsContainer.load(_xmlPath).RocketParts;
+            if (Instance == null)
+            {
+                Instance = this;
+                rocketParts = RocketPartsContainer.load(_xmlPath).RocketParts;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Spawners/ItemsSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Containers.RocketParts;
+using UnityEngine;
+
+public class ItemsSpawner : MonoBehaviour
+{
+    public List<GameObject> prefabs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var items = RocketPartsDatabase.Instance.rocketParts;
+        var lastPrefabSize = Vector3.zero;
+        
+        for (int i = 0; i < items.Count; i++)
+        {
+            var partPrefab = prefabs.Find((prefab) => prefab.name == items[i].name);
+            var instance = Instantiate(partPrefab, transform);
+            var size = instance.GetComponent<Collider>().bounds.size;
+            
+            if (i > 0)
+            {
+                instance.transform.position += new Vector3(size.x + lastPrefabSize.x,0,0);    
+            }
+
+            lastPrefabSize = size;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
